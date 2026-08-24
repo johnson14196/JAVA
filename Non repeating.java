8a35ac153e966f6365e793c4cad43dfddf7d9636
johnson14196 <son621339@gmail.com>
2026-08-24 09:36:09 +0530

    Create Non repeating.java

---
+++ Non repeating.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        String a = "maadeshwar";
+        int c = 0;
+        
+        for (int i = 0; i < a.length(); i++) {
+            c = 0;
+            for (int j = 0; j < a.length(); j++) {
+                if (a.charAt(i) == a.charAt(j)) {
+                    c++;
+                }
+            }
+            if (c == 1) {
+                System.out.println(a.charAt(i));
+                break; // Stops at the first non-repeating character ('d')
+            }
+        }
+    }
+}
